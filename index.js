2aa690d2e9e0f71961e280126b3fedac850a2f9d
Elaman Imashov <bobbyitkg@gmail.com>
2023-01-18 15:09:52 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+let myName = "Elaman";
+
+console.log(myName);
+console.log(myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.141593;
+
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+
+let example5 = 2 ** 4; // степень (квардат)
+let example6 = 81 ** (1/3); // кубический корень
+
+let example7 = 5 % 2; //остаток
+
+let example8 = "Hello " + 'World' + `!`;
+
+let example9 = 4 + 10 + 12;
+let example10 = 1 + 5 * 3;
+let example11 = 15 * (1 - 1);
+
+let example12 = 5 + '3';
+let example13 = '5' + 3;
+let example14 = '5' * 3; // 15
+let example15 = 'a' * 3; // NaN
+
+let example16 = -15;
+let example17 = +'15';
+
